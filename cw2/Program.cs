7d6660998352f11f3d81b0963f685e922906cee1
Michał Żabicki <s18803@pjwstk.edu.pl>
2020-03-09 10:53:36 +0100

    Commit

---
+++ cw2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var path= @"C:\Users\s18803\Desktop\dane.csv";
+
+            var liness= File.ReadLines(path);
+
+            foreach (var line in liness){
+                Console.WriteLine(line);
+            }
+
+            ICollection<string> list = new List<string>;
+
+            var today = DateTime.UtcNow; 
+
+            //var hash = new HashSet<Student>(new OwnCompare)
+
+ 
+        }
+    }
+}
+
+
+//.Newtonsoft.JSON   JSonConvert.SerializableObject
