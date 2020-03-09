03eb4a65196bdc0cf14880ac385d061fe09bb562
Michał Żabicki <s18803@pjwstk.edu.pl>
2020-03-09 11:18:13 +0100

    drugi commit

---
+++ cw2/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw2
+{
+    class Student
+    {
+        public string name {get; set; }
+        public string surname { get; set; }
+        public string studies { get; set; }
+        public string course { get; set; }
+        public string id { get; set; }
+        public string date { get; set; }
+        public string email { get; set; }
+        public string mother { get; set; }
+        public string father { get; set; }
+      
+    }
+}
--- cw2/Program.cs
+++ cw2/Program.cs
@@ -9,23 +9,42 @@ namespace Cw2
         static void Main(string[] args)
         {
 
-            var path= @"C:\Users\s18803\Desktop\dane.csv";
+          
+            var path = @"C:\Users\s18803\Desktop\dane.csv";
 
-            var liness= File.ReadLines(path);
+            var destinationPath = @"C:\Users\s18803\Desktop\wynik.xml";
 
-            foreach (var line in liness){
+            try
+            {
+        
+            
+             var liness = File.ReadLines(path);
+
+            foreach (var line in liness)
+            {
+
+
                 Console.WriteLine(line);
             }
 
-            ICollection<string> list = new List<string>;
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("{0}: ", e.GetType().Name, e.Message);
+            }
 
-            var today = DateTime.UtcNow; 
+            //ICollection<string> list = new List<string>;
+
+            var today = DateTime.UtcNow;
 
             //var hash = new HashSet<Student>(new OwnCompare)
 
- 
+
         }
     }
+
+
+
 }
 
 
