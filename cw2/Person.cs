24428d6c781719ff47e7ccccce09bba0f7f4e53c
Evitius <61686017+Evitius@users.noreply.github.com>
2020-03-13 13:58:01 +0100

    Update Person.cs

--- cw2/Person.cs
+++ cw2/Person.cs
@@ -15,6 +15,19 @@ namespace cw2
         public string Email { get; set; }
         public string Mother { get; set; }
         public string Father { get; set; }
-      
+
+
+        public Student(string [] line)
+        {
+            Name = line[0].ToString();
+            Surname = line[1].ToString();
+            Studies = line[2].ToString();
+            Course = line[3].ToString();
+            Id = line[4].ToString();
+            Date = line[5].ToString();
+            Email = line[6].ToString();
+            Mother = line[7].ToString();
+            Father = line[8].ToString();
+        }
     }
 }
